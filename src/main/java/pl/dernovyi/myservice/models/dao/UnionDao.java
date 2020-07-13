d9edec4f03dca100c547836c39effd6612987e39
serhiidernovyi <gifulmen@gmail.com>
2020-07-13 14:24:11 +0200

    monday commit

--- src/main/java/pl/dernovyi/myservice/models/dao/UnionDao.java
+++ src/main/java/pl/dernovyi/myservice/models/dao/UnionDao.java
@@ -1,18 +1,32 @@
 package pl.dernovyi.myservice.models.dao;
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name="union")
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sun.istack.NotNull;
+import lombok.*;
+import pl.dernovyi.myservice.models.dto.EmployeeDto;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="union")
+@EqualsAndHashCode(exclude = "name")
 public class UnionDao {
-    //    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    @NotNull
-//    @Column(name = "assoc_name", length = 50, nullable = false)
-//    private String associationName;
-//
-//    @ManyToMany(mappedBy = "union")
-//    private Set<Employee> employees;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @ManyToMany()
+    @JoinTable(name = "employee_union",
+            joinColumns = {@JoinColumn(name = "union_id", insertable = false, updatable = false, referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "employee_id", insertable = false, updatable = false, referencedColumnName = "id")}
+    )
+    @JsonBackReference
+    private Set<EmployeeDao> employees;
 }
